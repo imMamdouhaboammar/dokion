d6ecafbf676dc8da58d72ba83e9618b91b4f2a3f
Mamdouh Aboammar <58908124+imMamdouhaboammar@users.noreply.github.com>
2026-08-01 12:14:43 +0300

    feat(registry): define bounded package limits

---
+++ src/registry/package-limits.ts
@@ -0,0 +1,8 @@
+export const REGISTRY_PACKAGE_LIMITS = Object.freeze({
+  maximumArchiveBytes: 320 * 1024 * 1024,
+  maximumFiles: 9_999,
+  maximumFileBytes: 64 * 1024 * 1024,
+  maximumTotalPayloadBytes: 256 * 1024 * 1024,
+  maximumPathBytes: 255,
+  maximumManifestBytes: 4 * 1024 * 1024
+});
